6df605c9b922426fce7cfd3bdcb9b4d1eed03764
Aaron Freidus <aaronf101@gmail.com>
2017-06-18 17:03:04 -0400

    setting up env for dev vs lambda

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 const Twit = require('twit');
 
+if(process.env.NODE_ENV === 'dev') require('dotenv').config();
+
 let T = new Twit({
   consumer_key:         process.env.consumer_key,
   consumer_secret:      process.env.consumer_secret,
@@ -13,18 +15,21 @@ console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
-    console.log('value1 =', event.key1);
-    console.log('value2 =', event.key2);
-    console.log('value3 =', event.key3);
+    // console.log('value1 =', event.key1);
+    // console.log('value2 =', event.key2);
+    // console.log('value3 =', event.key3);
 
     /// https://dev.twitter.com/rest/reference/get/search/tweets
-    searchParams = {
+    let searchParams = {
       q: '',
       lang: 'en',
       result_type: 'popular',
       count: 100,
     }
-
-    callback(null, event.key1);  // Echo back the first key value
+    console.log(T)
+    
+    // callback(null, event.key1);  // Echo back the first key value
     //callback('Something went wrong');
 };
+
+exports.handler();
